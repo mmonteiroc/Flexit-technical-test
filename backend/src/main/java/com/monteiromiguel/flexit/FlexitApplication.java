eac74e35b2e12175d06c3c2d3995610fcc45f732
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-06-10 15:26:44 +0200

    Innitial git for the backend project

---
+++ backend/src/main/java/com/monteiromiguel/flexit/FlexitApplication.java
@@ -0,0 +1,13 @@
+package com.monteiromiguel.flexit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexitApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/monteiromiguel/flexit/FlexitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monteiromiguel.flexit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
