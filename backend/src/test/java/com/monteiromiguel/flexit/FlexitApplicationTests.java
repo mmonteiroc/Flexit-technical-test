985cc3a19d9806355dee2e0644af759f55fcd7cf
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-06-10 15:43:55 +0200

    We write 2 tests for now, one of get all so we know that the endpoint answers well the petition and one of findbyid

--- backend/src/test/java/com/monteiromiguel/flexit/FlexitApplicationTests.java
+++ backend/src/test/java/com/monteiromiguel/flexit/FlexitApplicationTests.java
@@ -1,13 +1,57 @@
 package com.monteiromiguel.flexit;
 
+import com.monteiromiguel.flexit.entity.Employee;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FlexitApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FlexitApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    private String getRootUrl() {
+        return "http://localhost:" + port;
+    }
+
+    @Test
+    public void testGetAllCars() {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/employees",
+                HttpMethod.GET, entity, String.class);
+
+        Assert.assertNotNull(response.getBody());
+    }
+
+    /**
+     * Here we test that we can fetch a single car using its id
+     */
+    @Test
+    public void testGetCarById() {
+        Employee employee = restTemplate.getForObject(getRootUrl() + "/employee/1", Employee.class);
+        System.out.println(employee);
+        Assert.assertNotNull(employee);
     }
 
 }
